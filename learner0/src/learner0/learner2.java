13d4162c8551598b1f9401226340d91d406ebd24
your name <your email>
2018-09-14 16:15:22 +0800

    first commit

---
+++ learner0/src/learner0/learner2.java
@@ -0,0 +1,13 @@
+package learner0;
+
+public class learner2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      String [] array=new String[]{"123","321"};
+      System.out.println(array[0]);
+      System.out.println(array[1]);
+      //System.out.println(array[-1]);
+	}
+
+}
